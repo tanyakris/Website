7a911da3931bec16e9073807871dc614f4cfc0bb
Tanya Krishnakumar <tanyakris@Tanyas-MBP.attlocal.net>
2014-11-26 01:37:22 -0800

    first commit

---
+++ subartsf/javascript/osx.js
@@ -0,0 +1,429 @@
+/*
+ * SimpleModal OSX Style Modal Dialog
+ * http://simplemodal.com
+ *
+ * Copyright (c) 2013 Eric Martin - http://ericmmartin.com
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx, a.osx").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content", self.container).show();
+				var title = $("#osx-modal-title", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close", self.container).show();
+								$("#osx-modal-data", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx1, a.osx1").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content1").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content1", self.container).show();
+				var title = $("#osx-modal-title1", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data1", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close1", self.container).show();
+								$("#osx-modal-data1", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx2, a.osx2").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content2").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content2", self.container).show();
+				var title = $("#osx-modal-title2", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data2", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close2", self.container).show();
+								$("#osx-modal-data2", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx3, a.osx3").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content3").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content3", self.container).show();
+				var title = $("#osx-modal-title3", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data3", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close3", self.container).show();
+								$("#osx-modal-data3", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx4, a.osx4").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content4").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content4", self.container).show();
+				var title = $("#osx-modal-title4", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data4", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close4", self.container).show();
+								$("#osx-modal-data4", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx5, a.osx5").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content5").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content5", self.container).show();
+				var title = $("#osx-modal-title5", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data5", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close5", self.container).show();
+								$("#osx-modal-data5", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
+
+jQuery(function ($) {
+	var OSX = {
+		container: null,
+		init: function () {
+			$("input.osx6, a.osx6").click(function (e) {
+				e.preventDefault();	
+
+				$("#osx-modal-content6").modal({
+					overlayId: 'osx-overlay',
+					containerId: 'osx-container',
+					closeHTML: null,
+					minHeight: 80,
+					opacity: 65, 
+					position: ['0',],
+					overlayClose: true,
+					onOpen: OSX.open,
+					onClose: OSX.close
+				});
+			});
+		},
+		open: function (d) {
+			var self = this;
+			self.container = d.container[0];
+			d.overlay.fadeIn('slow', function () {
+				$("#osx-modal-content6", self.container).show();
+				var title = $("#osx-modal-title6", self.container);
+				title.show();
+				d.container.slideDown('slow', function () {
+					setTimeout(function () {
+						var h = $("#osx-modal-data6", self.container).height()
+							+ title.height()
+							+ 20; // padding
+						d.container.animate(
+							{height: h}, 
+							200,
+							function () {
+								$("div.close6", self.container).show();
+								$("#osx-modal-data6", self.container).show();
+							}
+						);
+					}, 300);
+				});
+			})
+		},
+		close: function (d) {
+			var self = this; // this = SimpleModal object
+			d.container.animate(
+				{top:"-" + (d.container.height() + 20)},
+				500,
+				function () {
+					self.close(); // or $.modal.close();
+				}
+			);
+		}
+	};
+
+	OSX.init();
+
+});
